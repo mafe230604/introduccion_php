57da8b01fb3337ef3d9d61f61f850a4cd4f49ec0
futbolero06 <oscarardila@colegioguanenta.edu.co>
2023-07-18 07:55:29 -0500

    COmiii

---
+++ 3_operdores.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>comentarios</title>
+</head>
+<body>
+    <h1>comentarios en php<7h1>
+    <!--Formas deincluir codigo php-->
+
+    <?php
+    //Comentari de una linea 
+    /*Esto es un comentario de
+    varias lineas */
+    ?>
+    <!--
+    <%
+       codigo php
+    %>
+    <scrip" language="php">
+
+</script>-->
+</body>
+</html>
